17a81c133b9537e78de59859b435d44309fd55da
hwahle <helenwahle@gmail.com>
2015-10-29 12:13:32 -0700

    added questions in js

---
+++ js/script.js
@@ -0,0 +1,31 @@
+// JS objects 
+var questions = [{
+	question: "Copenhagen is the capital city of...",
+	qNum: 1,
+	choices: ["Sweden", "Denmark", "Norway", "Finland"],
+	answer: "Denmark",
+},	
+{
+	question: "Copenhagen is home to which Disney-adapted folk tale?",
+	qNum: 2,
+	choices: ["The Little Mermaid", "Beauty and the Beast", "Cinderella", "Lady and the Tramp"],
+	answer: "The Little Mermaid",
+},
+{
+	question: "Which great poet and fairytale writer was born in Copenhagen?",
+	qNum: 3,
+	choices: ["Brothers Grimm", "Lewis Carroll", "Wilhelm Hauff", "Hans Christian Andersen"],
+	answer: "Hans Christian Andersen",
+},
+{
+	question: "What is the name of the Best Restaurant in the world, located in Copenhagen?",
+	qNum: 4,
+	choices: ["Noma", "Geranium", "Cadeau", "The Paul"],
+	answer: "Noma",
+},
+{
+	question: "What does the Danish word 'Hygge' loosely translate to in English?",
+	qNum: 5,
+	choices: ["Chilly", "Drunk", "Cozy", "Authentic"],
+	answer: "Cozy",		
+}]
